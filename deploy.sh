031cca44240e5b0ae643da798ff932353c5b6a73
David Fazekas <fazekasda@gmail.com>
2017-06-13 20:36:35 +0200

    auto deploy with TravisCI

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,62 @@
 #/bin/bash
 
-invoke template.deploy
-invoke data.copy_deploy
-invoke data.export_protein_pages
-invoke dist_clone
-invoke deploy
-invoke clean_all
+#invoke template.deploy
+#invoke data.copy_deploy
+#invoke data.export_protein_pages
+#invoke dist_clone
+#invoke deploy
+#invoke clean_all
+
+###
+set -e
+
+# decrypt deploy key
+openssl aes-256-cbc -K $encrypted_56a395b73f83_key -iv $encrypted_56a395b73f83_iv -in travisci_rsa.enc -out travisci_rsa -d
+chmod 600 travisci_rsa
+eval `ssh-agent -s`
+ssh-add travisci_rsa
+rm travisci_rsa
+
+# config git
+git config user.name "Travis CI"
+git config user.email "$COMMIT_AUTHOR_EMAIL"
+git config push.default tracking
+GITHUB_REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
+GITHUB_USER=`echo $GITHUB_REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
+GITHUB_REPO=`echo $GITHUB_REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p'`
+GITHUB_REPO_SSH_URL="git@github.com:$GITHUB_USER/$GITHUB_REPO.git"
+git remote set-url origin $GITHUB_REPO_SSH_URL
+
+# generate template
+cd travisci_rsa
+npm install
+grunt build
+cp dist/js/app.js ../SalmoNet/themes/SalmoNet/static/js/app.js
+cp dist/css/style.css ../SalmoNet/themes/SalmoNet/static/css/style.css
+cp node_modules/uikit/dist/fonts/* ../SalmoNet/themes/SalmoNet/static/css/fonts/
+cd ..
+
+# process data
+cd scripts
+python deploy.py 
+cd ..
+
+# prepare deploy repo
+mkdir -p dist
+git clone git@github.com:TGAC/SalmoNet.git dist
+cd dist
+git checkout gh-pages
+git rm -rf *
+cd ..
+
+# generate static site
+cd SalmoNet
+hugo --uglyURLs
+cd ..
+
+# deploy
+cd dist
+git add --all
+git commit -am "automatic deploy"
+git push origin gh-pages
+cd ..
---
+++ scripts/deploy.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+import subprocess
+import os
+import json
+from .import_HC_data import import_HC_data, export_strain_select_json,\
+    export_strain_node_lists, export_protein_data
+
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+SalmoNetJson = "SalmoNet.json"
+temp_path = os.path.abspath(os.path.join(ROOT_PATH, os.pardir, "scripts","temp_data"))
+data_path = os.path.abspath(os.path.join(ROOT_PATH, os.pardir, "data"))
+dev_path = os.path.abspath(os.path.join(ROOT_PATH, os.pardir, "template","src","data"))
+deploy_path = os.path.abspath(os.path.join(ROOT_PATH, os.pardir,"SalmoNet","static","data"))
+download_path = os.path.abspath(os.path.join(ROOT_PATH, os.pardir,"SalmoNet","static","download"))
+pages_path = os.path.abspath(os.path.join(ROOT_PATH, os.pardir,"SalmoNet","content","protein"))
+
+# make dirs
+if not os.path.exists(temp_path):
+    os.makedirs(temp_path)
+if not os.path.exists(pages_path):
+    os.makedirs(pages_path)
+if not os.path.exists(download_path):
+    os.makedirs(download_path)
+if not os.path.exists(dev_path):
+    os.makedirs(dev_path)
+
+# clear temp
+subprocess.call("rm -rf %s/*" % temp_path, stdout=subprocess.PIPE)
+
+# import data
+SalmoNet = import_HC_data(os.path.join(data_path, "HC_nodes.csv"), os.path.join(data_path,"HC_interactions.csv"))
+with open(os.path.join(temp_path, SalmoNetJson), "w") as f:
+    json.dump(SalmoNet, f)
+
+# export strain nodes
+with open(os.path.join(temp_path, SalmoNetJson)) as data_file:
+    SalmoNet = json.load(data_file)
+    export_strain_node_lists(SalmoNet, temp_path)
+
+# export strain select
+with open(os.path.join(temp_path, SalmoNetJson)) as data_file:
+    SalmoNet = json.load(data_file)
+    export_strain_select_json(SalmoNet, os.path.join(temp_path, "strain_select.json"))
+
+# clear protein pages
+subprocess.call("rm -rf %s/*" % pages_path, stdout=subprocess.PIPE)
+
+# export protein pages
+with open(os.path.join(temp_path, SalmoNetJson)) as data_file:
+    SalmoNet = json.load(data_file)
+    export_protein_data(SalmoNet, pages_path, just_one)
+
+# copy deploy
+subprocess.call("cp %s %s" % (os.path.join(temp_path, "strain_select.json"), os.path.join(deploy_path, "strain_select.json")), stdout=subprocess.PIPE)
+subprocess.call("for file in %s*; do cp \"$file\" \"%s/$file\";done"  % (os.path.join(temp_path,"nodes"), deploy_path), stdout=subprocess.PIPE)
+subprocess.call("cp %s %s" % (os.path.join(data_path, "download"), download_path), stdout=subprocess.PIPE)
