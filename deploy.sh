a710fb9cdb87f0ab6651ac31213b9abc30934054
Balazs Bohar <bbazsi41@gmail.com>
2021-04-28 10:24:39 +0200

    [balazsbohar] Add deploy changes

--- deploy.sh
+++ deploy.sh
@@ -4,32 +4,32 @@
 set -e
 
 # decrypt deploy key
-# openssl aes-256-cbc -K $encrypted_56a395b73f83_key -iv $encrypted_56a395b73f83_iv -in travisci_rsa.enc -out travisci_rsa -d
-# chmod 600 travisci_rsa
-# eval `ssh-agent -s`
-# ssh-add travisci_rsa
-# rm travisci_rsa
+openssl aes-256-cbc -K $encrypted_56a395b73f83_key -iv $encrypted_56a395b73f83_iv -in travisci_rsa.enc -out travisci_rsa -d
+chmod 600 travisci_rsa
+eval `ssh-agent -s`
+ssh-add travisci_rsa
+rm travisci_rsa
 
 # config git
 # git config user.name "Travis CI"
 # git config user.email "$COMMIT_AUTHOR_EMAIL"
-# git config --global user.email "$COMMIT_AUTHOR_EMAIL"
-# git config --global user.name "Travis CI"
-# git config push.default tracking
-# GITHUB_REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
-# GITHUB_USER=`echo $GITHUB_REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
-# GITHUB_REPO=`echo $GITHUB_REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p'`
-# GITHUB_REPO_SSH_URL="git@github.com:$GITHUB_USER/$GITHUB_REPO.git"
-# git remote set-url origin $GITHUB_REPO_SSH_URL
+git config --global user.email "$COMMIT_AUTHOR_EMAIL"
+git config --global user.name "Travis CI"
+git config push.default tracking
+GITHUB_REPO_URL=`git remote -v | grep -m1 '^origin' | sed -Ene's#.*(https://[^[:space:]]*).*#\1#p'`
+GITHUB_USER=`echo $GITHUB_REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\1#p'`
+GITHUB_REPO=`echo $GITHUB_REPO_URL | sed -Ene's#https://github.com/([^/]*)/(.*).git#\2#p'`
+GITHUB_REPO_SSH_URL="git@github.com:$GITHUB_USER/$GITHUB_REPO.git"
+git remote set-url origin $GITHUB_REPO_SSH_URL
 
 # generate template
-# cd template
-# npm install
-# grunt build
-# cp dist/js/app.js ../SalmoNet/themes/SalmoNet/static/js/app.js
-# cp dist/css/style.css ../SalmoNet/themes/SalmoNet/static/css/style.css
-# cp node_modules/uikit/dist/fonts/* ../SalmoNet/themes/SalmoNet/static/css/fonts/
-# cd ..
+cd template
+npm install
+grunt build
+cp dist/js/app.js ../SalmoNet/themes/SalmoNet/static/js/app.js
+cp dist/css/style.css ../SalmoNet/themes/SalmoNet/static/css/style.css
+cp node_modules/uikit/dist/fonts/* ../SalmoNet/themes/SalmoNet/static/css/fonts/
+cd ..
 
 # process data
 echo " 1/5 - process data"
